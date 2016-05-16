6de391697a55cb266af0a754c076d91c9bf663b8
CarlOlsson <carlolsson.co@gmail.com>
2016-05-16 10:00:10 +0200

    fixed param bug

--- EKF/control.cpp
+++ EKF/control.cpp
@@ -535,7 +535,7 @@ void Ekf::controlMagAiding()
 	}
 
 	// Determine if we want to fuse syntetic sideslip measurement
-	bool beta_fusion_time_triggered = _time_last_imu - _time_last_beta_fuse > beta_avg_ft_ms;
+	bool beta_fusion_time_triggered = _time_last_imu - _time_last_beta_fuse > _params.beta_avg_ft_ms;
 	bool beta_fusion_required = _time_last_imu - _time_last_pos_fuse > 7e6;
 	bool beta_fusion_feasible = _control_status.flags.wind;
 
