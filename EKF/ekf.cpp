3f879554d3d4285e2641f1c44391fc10fca745d8
CarlOlsson <carlolsson.co@gmail.com>
2016-06-07 07:44:00 +0200

    activate tas, disabled by default

--- EKF/ekf.cpp
+++ EKF/ekf.cpp
@@ -365,9 +365,8 @@ bool Ekf::update()
 			_fuse_flow = false;
 		}
 
-		// TODO This is just to get the logic inside but we will only start fusion once we tested this again
-		//if (_airspeed_buffer.pop_first_older_than(_imu_sample_delayed.time_us, &_airspeed_sample_delayed)) {
-		if (false) {
+		// Fuse true airspeed measurement
+		if (_airspeed_buffer.pop_first_older_than(_imu_sample_delayed.time_us, &_airspeed_sample_delayed)) {
 			fuseAirspeed();
 		}
 	}
